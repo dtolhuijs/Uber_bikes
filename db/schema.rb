17100c6f0600e950402e22fac76ed4ddc192f472
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-13 17:14:26 +0200

    change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160613123910) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: :cascade do |t|
     t.string   "start_date"
     t.string   "end_date"
