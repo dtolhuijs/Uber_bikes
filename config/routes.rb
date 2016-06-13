d859eed1cfacd1a126339911c5b1e9fae32195c7
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-13 15:44:53 +0200

    Make products#index the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  resources :users
+  resources :products
+  resources :bookings
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
