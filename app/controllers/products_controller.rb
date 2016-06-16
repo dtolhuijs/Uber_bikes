f2219b340c8df215f3b3c85e1ca8c06b2eeb8741
Vincent Brinkman <vincentbrinkman@yahoo.com>
2016-06-16 14:40:23 +0200

    controllers for user and products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,53 @@ class ProductsController < ApplicationController
       @products = Product.all
    end
 
+   def edit
+        @product = Product.find( params[:id] )
+      end
+
+  def update
+        @product = Product.find( params[:id] )
+
+        if @product.update_attributes( product_params )
+       redirect_to @product
+     else
+       render 'edit'
+     end
+   end
+
    def new
      @product = Product.new
+     @novel.product_id = params[:product_id]
   end
 
    def show
          @product = Product.find( params[:id] )
       end
 
-end
+      def create
+          product = Product.new( product_params )
+
+          if product.save
+             redirect_to author_path( product.author_id )
+          else
+             render new_product_path
+          end
+       end
+
+       def destroy
+         @product = Product.find( params[:id] )
+
+         author_id = @product.author_id
+
+         @product.destroy
+
+         redirect_to product_path( product_id )
+       end
+
+    private
+
+       def product_params
+         params.require( :product ).permit( :users, :name, :location, :email, :phone, product_ids: [] )
+       end
+
+    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,49 @@
 class UsersController < ApplicationController
   def index
-      @user = User.all
+      @users = User.all
     end
+  def new
+        @user = User.new
+     end
+  def show
+    @user = User.find( params[:id] )
+  def edit
+          @user = User.find( params[:id] )
+       end
 
+  def update
+             @user = User.find( params[:id] )
+
+             if @user.update_attributes( author_params )
+                redirect_to @user
+             else
+                render 'edit'
+             end
+          end
+
+  def create
+          user = User.new( author_params )
+
+           if user.save
+              redirect_to users_path
+           else
+              render 'new'
+           end
+        end
+
+  def destroy
+             @user = User.find( params[:id] )
+
+             @user.destroy
+
+             redirect_to users_path
+          end
+
+  def author_params
+                params.require( :user ).permit( :name, :country, :bio )
+             end
+
+          end
 
 
 end
