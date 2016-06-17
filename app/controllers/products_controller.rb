af4d15fe8140fa9ad442471e4200b331d8496b3a
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-17 11:25:52 +0200

    added carrierwave

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,7 +50,7 @@ class ProductsController < ApplicationController
     private
 
        def product_params
-         params.require( :product ).permit( :users, :title, :description, :location, product_ids: [] )
+         params.require( :product ).permit( :users, :title, :description, :location,:image, product_ids: [] )
        end
 
     end
