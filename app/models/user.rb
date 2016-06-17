c0bd1687532f7decbb4a20a9d88e552088dc43e6
GNelson1973 <jorge.oosthoek@gmail.com>
2016-06-17 11:37:40 +0200

    resolve further merge conflicts in gemfile and users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :products
-  has_many :bookings
-=======
   has_many :products, dependent: :destroy
   has_many :bookings, dependent: :destroy
->>>>>>> 21f345ba0c1c6a1973f14e79960110616251640f
 
 end
